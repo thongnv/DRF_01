575b66aca4a2249579dea5d190769c696502234c
tom nguyen <thongola@hotmail.com>
2016-10-24 22:19:51 +0700

    add app accout

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'django.contrib.gis',
+    'rest_framework',
+    'app.account'
 )
 
 MIDDLEWARE_CLASSES = (
